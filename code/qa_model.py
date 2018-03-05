7d6773c5b05ab2c1810324f49c40f0a2a570e6d3
minfawang <minfa.wang@gmail.com>
2018-03-04 18:17:05 -0800

    qs model with end-dep-start-add

--- code/qa_model.py
+++ code/qa_model.py
@@ -30,7 +30,7 @@ from tensorflow.python.ops import embedding_ops
 from evaluate import exact_match_score, f1_score
 from data_batcher import get_batch_generator
 from pretty_print import print_example
-from modules import RNNEncoder, SimpleSoftmaxLayer, BasicAttn
+from modules import RNNEncoder, SimpleSoftmaxLayer, BasicAttn, MulAttn
 
 logging.basicConfig(level=logging.INFO)
 
@@ -137,27 +137,89 @@ class QAModel(object):
         # Use context hidden states to attend to question hidden states
         attn_layer = BasicAttn(self.keep_prob, self.FLAGS.hidden_size*2, self.FLAGS.hidden_size*2)
         _, attn_output = attn_layer.build_graph(question_hiddens, self.qn_mask, context_hiddens) # attn_output is shape (batch_size, context_len, hidden_size*2)
+        # attn_layer = MulAttn(self.keep_prob, self.FLAGS.hidden_size*2, self.FLAGS.hidden_size*2)
+        # _, _, attn_output = attn_layer.build_graph(question_hiddens, self.qn_mask, context_hiddens) # attn_output is shape (batch_size, context_len, hidden_size*2)
 
         # Concat attn_output to context_hiddens to get blended_reps
         blended_reps = tf.concat([context_hiddens, attn_output], axis=2) # (batch_size, context_len, hidden_size*4)
 
-        # Apply fully connected layer to each blended representation
-        # Note, blended_reps_final corresponds to b' in the handout
-        # Note, tf.contrib.layers.fully_connected applies a ReLU non-linarity here by default
-        blended_reps_final = tf.contrib.layers.fully_connected(blended_reps, num_outputs=self.FLAGS.hidden_size) # blended_reps_final is shape (batch_size, context_len, hidden_size)
+        def shared_output_nn(blended_reps_se, reuse=None):
+            with tf.variable_scope('FirstHidden') as hidden_scope:
+                blended_reps_final = tf.contrib.layers.fully_connected(
+                    blended_reps_se,
+                    num_outputs=self.FLAGS.hidden_size,
+                    reuse=reuse,
+                    scope=hidden_scope,
+                ) # blended_reps_final is shape (batch_size, context_len, hidden_size)
+
+            with tf.variable_scope('SimpleSoftmax') as softmax_scope:
+                softmax_layer = SimpleSoftmaxLayer()
+                # Both of shape (batch_size, context_len)
+                return softmax_layer.build_graph(
+                    blended_reps_final, self.context_mask,
+                    reuse=reuse,
+                    scope=softmax_scope,
+                )
 
         # Use softmax layer to compute probability distribution for start location
         # Note this produces self.logits_start and self.probdist_start, both of which have shape (batch_size, context_len)
-        with vs.variable_scope("StartDist"):
-            softmax_layer_start = SimpleSoftmaxLayer()
-            self.logits_start, self.probdist_start = softmax_layer_start.build_graph(blended_reps_final, self.context_mask)
+        with tf.variable_scope('StartEnd') as se_scope:
+            self.logits_start, self.probdist_start = shared_output_nn(blended_reps, reuse=False)
+            # se_scope.reuse_variables()
 
-        # Use softmax layer to compute probability distribution for end location
-        # Note this produces self.logits_end and self.probdist_end, both of which have shape (batch_size, context_len)
-        with vs.variable_scope("EndDist"):
-            softmax_layer_end = SimpleSoftmaxLayer()
-            self.logits_end, self.probdist_end = softmax_layer_end.build_graph(blended_reps_final, self.context_mask)
+            probdist_start_t = tf.expand_dims(self.probdist_start, -1)  # shape (batch_size, context_len, 1)
+            attn_start = tf.reduce_sum(probdist_start_t * blended_reps, axis=1, keep_dims=True)  # shape (batch_size, 1, hidden_size * 4)
+            # attn_start = tf.tile(attn_start, [1, self.FLAGS.context_len, 1])  # shape (batch_size, context_len, hidden_size * 4)
+            blended_reps_end = tf.add(blended_reps, attn_start, name='blended_reps_end')  # shape (batch_size, context_len, hidden_size * 4)
+            tf.assert_equal(tf.shape(blended_reps_end), tf.shape(blended_reps), name='assert_blend_reps_end_shape')
+            self.logits_end, self.probdist_end = shared_output_nn(blended_reps_end, reuse=True)
 
+        # ##### Extend dimensions #####
+        # # Use softmax layer to compute probability distribution for start location
+        # # Note this produces self.logits_start and self.probdist_start, both of which have shape (batch_size, context_len)
+        # with tf.variable_scope('StartEnd') as se_scope:
+        #     zero_rep = tf.zeros((tf.shape(blended_reps)[0], self.FLAGS.context_len, self.FLAGS.hidden_size * 4))
+        #     blended_reps_start = tf.concat([blended_reps, zero_rep], axis=2, name='blended_reps_start')  # shape (batch_size, context_len, hidden_size * 8)
+        #     tf.assert_equal(
+        #         tf.shape(blended_reps_start),
+        #         [tf.shape(blended_reps)[0], self.FLAGS.context_len, self.FLAGS.hidden_size * 8],
+        #         name='assert_blend_reps_start_shape')
+        #     self.logits_start, self.probdist_start = shared_output_nn(blended_reps_start, reuse=False)
+        #     # se_scope.reuse_variables()
+        #
+        #     probdist_start_t = tf.expand_dims(self.probdist_start, -1)  # shape (batch_size, context_len, 1)
+        #     attn_start = tf.reduce_sum(probdist_start_t * blended_reps, axis=1, keep_dims=True)  # shape (batch_size, 1, hidden_size * 4)
+        #     attn_start = tf.tile(attn_start, [1, self.FLAGS.context_len, 1])  # shape (batch_size, context_len, hidden_size * 4)
+        #     blended_reps_end = tf.concat([blended_reps, attn_start], axis=2, name='blended_reps_end')  # shape (batch_size, context_len, hidden_size * 8)
+        #     tf.assert_equal(tf.shape(blended_reps_end), tf.shape(blended_reps_start), name='assert_blend_reps_end_shape')
+        #     self.logits_end, self.probdist_end = shared_output_nn(blended_reps_end, reuse=True)
+
+
+        # # Apply fully connected layer to each blended representation
+        # # Note, blended_reps_final corresponds to b' in the handout
+        # # Note, tf.contrib.layers.fully_connected applies a ReLU non-linarity here by default
+        # blended_reps_final = tf.contrib.layers.fully_connected(blended_reps, num_outputs=self.FLAGS.hidden_size) # blended_reps_final is shape (batch_size, context_len, hidden_size)
+        #
+        # # Use softmax layer to compute probability distribution for start location
+        # # Note this produces self.logits_start and self.probdist_start, both of which have shape (batch_size, context_len)
+        # with vs.variable_scope("StartDist"):
+        #     softmax_layer_start = SimpleSoftmaxLayer()
+        #     self.logits_start, self.probdist_start = softmax_layer_start.build_graph(blended_reps_final, self.context_mask)
+        #
+        # # Use softmax layer to compute probability distribution for end location
+        # # Note this produces self.logits_end and self.probdist_end, both of which have shape (batch_size, context_len)
+        # with vs.variable_scope("EndDist"):
+        #     ### baseline ###
+        #     softmax_layer_end = SimpleSoftmaxLayer()
+        #     self.logits_end, self.probdist_end = softmax_layer_end.build_graph(blended_reps_final, self.context_mask)
+        #
+        #     ### Failed Attempt1 ###
+        #     # logits_start = tf.expand_dims(self.logits_start, -1)  # shape (batch_size, context_len, 1)
+        #     # attn_start = tf.reduce_sum(logits_start * blended_reps_final, axis=1, keep_dims=True)  # shape (batch_size, 1, hidden_size)
+        #     # mul_attn_layer_end = MulAttn(self.keep_prob, self.FLAGS.hidden_size, self.FLAGS.hidden_size)
+        #     # self.logits_end, self.probdist_end, _ = mul_attn_layer_end.build_graph(blended_reps_final, self.context_mask, attn_start)  # shape (batch_size, 1, context_len)
+        #     # self.logits_end = tf.squeeze(self.logits_end, [1])
+        #     # self.probdist_end = tf.squeeze(self.probdist_end, [1])
 
     def add_loss(self):
         """
