d91500bde0d689391e2ee9477a6a26d9c02ddc6e
minfawang <minfa.wang@gmail.com>
2018-03-04 18:16:25 -0800

    Add MulAttn. Plus patch to softmax layer.

--- code/modules.py
+++ code/modules.py
@@ -49,7 +49,7 @@ class RNNEncoder(object):
         self.rnn_cell_bw = rnn_cell.GRUCell(self.hidden_size)
         self.rnn_cell_bw = DropoutWrapper(self.rnn_cell_bw, input_keep_prob=self.keep_prob)
 
-    def build_graph(self, inputs, masks):
+    def build_graph(self, inputs, masks, initial_state_fw=None, initial_state_bw=None):
         """
         Inputs:
           inputs: Tensor shape (batch_size, seq_len, input_size)
@@ -66,7 +66,14 @@ class RNNEncoder(object):
 
             # Note: fw_out and bw_out are the hidden states for every timestep.
             # Each is shape (batch_size, seq_len, hidden_size).
-            (fw_out, bw_out), _ = tf.nn.bidirectional_dynamic_rnn(self.rnn_cell_fw, self.rnn_cell_bw, inputs, input_lens, dtype=tf.float32)
+            (fw_out, bw_out), _ = tf.nn.bidirectional_dynamic_rnn(
+                self.rnn_cell_fw,
+                self.rnn_cell_bw,
+                inputs,
+                input_lens,
+                dtype=tf.float32,
+                initial_state_fw=initial_state_fw,
+                initial_state_bw=initial_state_bw)
 
             # Concatenate the forward and backward hidden states
             out = tf.concat([fw_out, bw_out], 2)
@@ -86,7 +93,7 @@ class SimpleSoftmaxLayer(object):
     def __init__(self):
         pass
 
-    def build_graph(self, inputs, masks):
+    def build_graph(self, inputs, masks, scope=None, reuse=None):
         """
         Applies one linear downprojection layer, then softmax.
 
@@ -106,7 +113,8 @@ class SimpleSoftmaxLayer(object):
         with vs.variable_scope("SimpleSoftmaxLayer"):
 
             # Linear downprojection layer
-            logits = tf.contrib.layers.fully_connected(inputs, num_outputs=1, activation_fn=None) # shape (batch_size, seq_len, 1)
+            logits = tf.contrib.layers.fully_connected(
+                inputs, num_outputs=1, activation_fn=None, reuse=reuse, scope=scope) # shape (batch_size, seq_len, 1)
             logits = tf.squeeze(logits, axis=[2]) # shape (batch_size, seq_len)
 
             # Take softmax over sequence
@@ -174,6 +182,80 @@ class BasicAttn(object):
             output = tf.nn.dropout(output, self.keep_prob)
 
             return attn_dist, output
+
+
+class MulAttn(object):
+    """Module for multiplicative attention.
+
+    Note: in this module we use the terminology of "keys" and "values" (see lectures).
+    In the terminology of "X attends to Y", "keys attend to values".
+
+    In the baseline model, the keys are the context hidden states
+    and the values are the question hidden states.
+
+    We choose to use general terminology of keys and values in this module
+    (rather than context and question) to avoid confusion if you reuse this
+    module with other inputs.
+    """
+
+    def __init__(self, keep_prob, key_vec_size, value_vec_size):
+        """
+        Inputs:
+          keep_prob: tensor containing a single scalar that is the keep probability (for dropout)
+          key_vec_size: size of the key vectors. int
+          value_vec_size: size of the value vectors. int
+        """
+        self.keep_prob = keep_prob
+        self.key_vec_size = key_vec_size
+        self.value_vec_size = value_vec_size
+
+    def build_graph(self, values, values_mask, keys):
+        """
+        Keys attend to values.
+        For each key, return an attention distribution and an attention output vector.
+
+        Inputs:
+          values: Tensor shape (batch_size, num_values, value_vec_size).
+          values_mask: Tensor shape (batch_size, num_values).
+            1s where there's real input, 0s where there's padding
+          keys: Tensor shape (batch_size, num_keys, key_vec_size)
+
+        Outputs:
+          attn_logits: Tensor shape (batch_size, num_keys, num_values).
+            The logits of attn_dist.
+          attn_dist: Tensor shape (batch_size, num_keys, num_values).
+            For each key, the distribution should sum to 1,
+            and should be 0 in the value locations that correspond to padding.
+          output: Tensor shape (batch_size, num_keys, hidden_size).
+            This is the attention output; the weighted sum of the values
+            (using the attention distribution as weights).
+        """
+        with vs.variable_scope("MulAttn"):
+            # Calculate attention distribution
+            # TODO(minfa): Check initialization of W_attn. Did they inherit
+            # default initialization method or are they initialized as zeros.
+            W_attn = tf.get_variable('W_attn', shape=(self.value_vec_size, self.key_vec_size))
+            tf.summary.histogram('W_attn', W_attn)
+
+            keys_t = tf.transpose(keys, perm=[2, 0, 1])  # shape (key_vec_size, batch_size, num_keys)
+            keys_t = tf.reshape(keys_t, (self.key_vec_size, -1))  # shape (key_vec_size, batch_size * num_keys)
+            attn_logits = tf.matmul(W_attn, keys_t)  # shape (value_vec_size, batch_size * num_keys)
+            attn_logits = tf.reshape(attn_logits, (self.value_vec_size, tf.shape(keys)[0], tf.shape(keys)[1]))  # shape (value_vec_size, batch_size, num_keys)
+            attn_logits = tf.transpose(attn_logits, perm=[1, 2, 0])  # shape (batch_size, num_keys, value_vec_size)
+
+            values_t = tf.transpose(values, perm=[0, 2, 1]) # (batch_size, value_vec_size, num_values)
+            attn_logits = tf.matmul(attn_logits, values_t) # shape (batch_size, num_keys, num_values)
+
+            attn_logits_mask = tf.expand_dims(values_mask, 1) # shape (batch_size, 1, num_values)
+            attn_logits, attn_dist = masked_softmax(attn_logits, attn_logits_mask, 2) # shape (batch_size, num_keys, num_values). take softmax over values
+
+            # Use attention distribution to take weighted sum of values
+            output = tf.matmul(attn_dist, values) # shape (batch_size, num_keys, value_vec_size)
+
+            # Apply dropout
+            output = tf.nn.dropout(output, self.keep_prob)
+
+            return attn_logits, attn_dist, output
 
 
 def masked_softmax(logits, mask, dim):
