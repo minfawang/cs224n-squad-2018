1850d20dbb0038df2f5847959a05ebe9922e66bb
minfawang <minfa.wang@gmail.com>
2018-03-08 22:14:18 -0800

    Add bidaf_edsc3_model

--- code/main.py
+++ code/main.py
@@ -28,7 +28,8 @@ import tensorflow as tf
 # from qa_model import QAModel
 # from edsa_model import QAModel
 # from edsc_model import QAModel
-from edsc2_model import QAModel
+# from edsc2_model import QAModel
+from bidaf_edsc2_model import QAModel
 from vocab import get_glove
 from official_eval_helper import get_json_data, generate_answers
 
