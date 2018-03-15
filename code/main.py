7512cddccf8658bd43c4ba4efa497e5134dbbb0e
Minfa <minfa.wang@gmail.com>
2018-03-15 16:54:06 -0700

    Use bico model

--- code/main.py
+++ code/main.py
@@ -31,7 +31,8 @@ import tensorflow as tf
 # from edsc2_model import QAModel
 # from bidaf_model import QAModel
 # from bidaf_self_model import QAModel
-from coattn_model import QAModel
+# from coattn_model import QAModel
+from bi_co_attn_model import QAModel
 from vocab import get_glove
 from official_eval_helper import get_json_data, generate_answers
 
