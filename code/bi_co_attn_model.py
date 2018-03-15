1e376dd6d7c1623f839f391e6a98f9b914855420
Minfa <minfa.wang@gmail.com>
2018-03-15 16:56:13 -0700

    Fix import

--- code/bi_co_attn_model.py
+++ code/bi_co_attn_model.py
@@ -31,7 +31,7 @@ from tensorflow.python.ops import embedding_ops
 from evaluate import exact_match_score, f1_score
 from data_batcher import get_batch_generator
 from pretty_print import print_example
-from modules import RNNEncoder, SimpleSoftmaxLayer, BasicAttn, MulAttn, BidafAttn, SelfAttn
+from modules import RNNEncoder, SimpleSoftmaxLayer, BasicAttn, MulAttn, BidafAttn, SelfAttn, CoAttn
 
 logging.basicConfig(level=logging.INFO)
 
